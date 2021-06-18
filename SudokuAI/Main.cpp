04e3de117d9c9267b8c907663c44948069996c57
Pawel Tarsala (PLT) <PLT@kmd.dk>
2021-06-18 10:41:05 +0200

    fix versions and formatting

--- SudokuAI/Main.cpp
+++ SudokuAI/Main.cpp
@@ -9,38 +9,30 @@
 #include<vector>
 #include <random>
 
-
 #define ILOSC_ELEMENTOW 81
 #define N 2000000
 using namespace std;
 
 /*
 
-
 - okreslenie wszystkich mozliwych cyfr w kazdym wolnym kwadracie
-- wyszukiwanie rozwiazan metoda min-max z alpha-beta prunning 
+- wyszukiwanie rozwiazan metoda min-max z alpha-beta prunning
 - dodac sensowne dodawanie rozwiazanego sudoku, zeby pozniej nie sprawdzac wszystkich czy juz bylo takie sudoku
 
 */
 
-
 unsigned long long  ILOSC_ROZWIAZANYCH_SUDOKU = 0;
-
-
 
 struct Rozwiazania
 {
 	Rozwiazania() {
-
 	}
 	Rozwiazania(unsigned Sudoku[9][9]) {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				sudoku[i][j] = Sudoku[i][j];
-
 			}
 		}
-
 	}
 	unsigned sudoku[9][9];
 };
@@ -50,7 +42,6 @@ struct TrudneSudoku {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				sudoku[i][j] = 0;
-
 			}
 		}
 
@@ -61,7 +52,6 @@ struct TrudneSudoku {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				sudoku[i][j] = Sudoku[i][j];
-
 			}
 		}
 		liczbaCyfrWejsciowych = LiczbaCyfrWejsciowych;
@@ -72,32 +62,28 @@ struct TrudneSudoku {
 	unsigned sudoku[9][9];
 };
 
-
 struct MozliweCyfry {
 	vector<unsigned int > cyfryZajete;
 	vector<unsigned int > cyfryDoUzycia;
 	unsigned wiersz, kolumna;
 };
 
-
 vector < Rozwiazania > tablica(N);
 vector < TrudneSudoku > trudneSudoku(N);
 
 vector <MozliweCyfry > mozliweCyfry(ILOSC_ELEMENTOW);
 
-
 void wypelnij(unsigned sudoku[9][9])
 {
-	for (int wiersz = 0; wiersz<9; wiersz++)
-		for (int kolumna = 0; kolumna<9; kolumna++)
+	for (int wiersz = 0; wiersz < 9; wiersz++)
+		for (int kolumna = 0; kolumna < 9; kolumna++)
 			cin >> sudoku[wiersz][kolumna];
 }
 
-
 void wypelnij0(unsigned sudoku[9][9])
 {
-	for (int wiersz = 0; wiersz<9; wiersz++)
-		for (int kolumna = 0; kolumna<9; kolumna++)
+	for (int wiersz = 0; wiersz < 9; wiersz++)
+		for (int kolumna = 0; kolumna < 9; kolumna++)
 			sudoku[wiersz][kolumna] = 0;
 }
 
@@ -109,9 +95,9 @@ void wypisz(unsigned sudoku[9][9])
 
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	for (unsigned wiersz = 0; wiersz<9; wiersz++)
+	for (unsigned wiersz = 0; wiersz < 9; wiersz++)
 	{
-		for (unsigned kolumna = 0; kolumna<9; kolumna++)
+		for (unsigned kolumna = 0; kolumna < 9; kolumna++)
 		{
 			if (sudoku[wiersz][kolumna] == 0)
 			{
@@ -123,19 +109,17 @@ void wypisz(unsigned sudoku[9][9])
 				SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 				cout << sudoku[wiersz][kolumna] << " ";
 			}
-
 		}
 		cout << endl;
 	}
-
 }
 
 void stale(unsigned sudoku[9][9], unsigned tablica[9][9])
 {
 	wypelnij0(tablica);
-	for (unsigned wiersz = 0; wiersz<9; wiersz++)
+	for (unsigned wiersz = 0; wiersz < 9; wiersz++)
 	{
-		for (unsigned kolumna = 0; kolumna<9; kolumna++)
+		for (unsigned kolumna = 0; kolumna < 9; kolumna++)
 		{
 			if (sudoku[wiersz][kolumna] != 0)
 			{
@@ -145,45 +129,42 @@ void stale(unsigned sudoku[9][9], unsigned tablica[9][9])
 	}
 }
 
-void wypisz(vector<Rozwiazania> &rozwiazania) {
+void wypisz(vector<Rozwiazania>& rozwiazania) {
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 	cout << "\n\n\n";
 	unsigned licznik = 0;
-	for (unsigned i = 0; i<ILOSC_ROZWIAZANYCH_SUDOKU; i++)
+	for (unsigned i = 0; i < ILOSC_ROZWIAZANYCH_SUDOKU; i++)
 	{
 		cout << endl << licznik + 1 << ". " << endl;
 		for (unsigned wiersz = 0; wiersz < 9; wiersz++)
 		{
-			for (unsigned kolumna = 0; kolumna<9; kolumna++)
+			for (unsigned kolumna = 0; kolumna < 9; kolumna++)
 			{
-				
-					SetConsoleTextAttribute(hOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-					cout << tablica[i].sudoku[wiersz][kolumna] << " ";
-					SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+				SetConsoleTextAttribute(hOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+				cout << tablica[i].sudoku[wiersz][kolumna] << " ";
+				SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 			}
 			cout << endl;
 		}
 		licznik++;
 	}
 }
-void wypisz(vector<TrudneSudoku> &rozwiazania,unsigned granicaDolna, unsigned granicaGorna) {
+void wypisz(vector<TrudneSudoku>& rozwiazania, unsigned granicaDolna, unsigned granicaGorna) {
 	HANDLE hOut;
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
 	cout << "\n\n\n";
 	unsigned licznik = 0;
-	for (unsigned i = granicaDolna; i<granicaGorna; i++)
+	for (unsigned i = granicaDolna; i < granicaGorna; i++)
 	{
-
 		cout << endl << licznik + 1 << ".\n LiczbaCyfr : " << ::trudneSudoku[i].liczbaCyfrWejsciowych;
 		cout << endl << "Liczba Rozwiazan : " << ::trudneSudoku[i].liczbaRozwiazan << endl;
 		for (unsigned wiersz = 0; wiersz < 9; wiersz++)
 		{
-			for (unsigned kolumna = 0; kolumna<9; kolumna++)
+			for (unsigned kolumna = 0; kolumna < 9; kolumna++)
 			{
-
 				SetConsoleTextAttribute(hOut, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
 				cout << trudneSudoku[i].sudoku[wiersz][kolumna] << " ";
 				SetConsoleTextAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
@@ -196,11 +177,10 @@ void wypisz(vector<TrudneSudoku> &rozwiazania,unsigned granicaDolna, unsigned gr
 
 bool CzyByla(vector< unsigned int >& tablica, unsigned cyfra)
 {
-
 	if (tablica.size() == 0)
 		return false;
 
-	for (unsigned i = 0; i< tablica.size(); i++)
+	for (unsigned i = 0; i < tablica.size(); i++)
 		if (cyfra == tablica[i])
 			return true;
 
@@ -209,7 +189,7 @@ bool CzyByla(vector< unsigned int >& tablica, unsigned cyfra)
 
 void wypelijCyfryDoUzycia(vector< unsigned int >& zajete, vector< unsigned int >& doUzycia) {
 	bool jest = false;
-	for (int i = 1; i <=9; i++) {
+	for (int i = 1; i <= 9; i++) {
 		bool jest = false;
 		for (int j = 0; j < zajete.size(); j++) {
 			if (zajete[j] == i) {
@@ -217,54 +197,49 @@ void wypelijCyfryDoUzycia(vector< unsigned int >& zajete, vector< unsigned int >
 				break;
 			}
 		}
-		if(!jest)
-		doUzycia.push_back(i);
+		if (!jest)
+			doUzycia.push_back(i);
 	}
 }
 
 void MetodaKratkowa(unsigned sudoku[9][9], vector< unsigned int >& Npowt, unsigned wiersz, unsigned kolumna)
 {
-
-	for (unsigned k = 0; k<9; k++)
+	for (unsigned k = 0; k < 9; k++)
 		if (CzyByla(Npowt, sudoku[wiersz][k]) == false)
 		{
 			Npowt.push_back(sudoku[wiersz][k]);
 		}
 
-	for (unsigned w = 0; w<9; w++)
+	for (unsigned w = 0; w < 9; w++)
 		if (CzyByla(Npowt, sudoku[w][kolumna]) == false)
 		{
 			Npowt.push_back(sudoku[w][kolumna]);
 		}
 
-
-	for (unsigned w2 = (wiersz / 3) * 3; w2<((wiersz / 3) * 3 + 3); w2++)
-		for (unsigned k2 = (kolumna / 3) * 3; k2<((kolumna / 3) * 3 + 3); k2++)
+	for (unsigned w2 = (wiersz / 3) * 3; w2 < ((wiersz / 3) * 3 + 3); w2++)
+		for (unsigned k2 = (kolumna / 3) * 3; k2 < ((kolumna / 3) * 3 + 3); k2++)
 			if (CzyByla(Npowt, sudoku[w2][k2]) == false)
 			{
 				Npowt.push_back(sudoku[w2][k2]);
 			}
-
-
-
 }
 
 void nadpisz(unsigned sudoku[9][9], unsigned sudoku1[9][9])
 {
-	for (int wiersz = 0; wiersz<9; wiersz++)
-		for (int kolumna = 0; kolumna<9; kolumna++)
+	for (int wiersz = 0; wiersz < 9; wiersz++)
+		for (int kolumna = 0; kolumna < 9; kolumna++)
 			sudoku1[wiersz][kolumna] = sudoku[wiersz][kolumna];
 }
 
 void zapisz(Rozwiazania tablica[], unsigned ilosc)
 {
 	ofstream wyjscie("sudoku.txt");
-	for (unsigned i = 0; i<ilosc; i++)
+	for (unsigned i = 0; i < ilosc; i++)
 	{
 		wyjscie << i + 1 << ". " << endl;
-		for (unsigned wiersz = 0; wiersz<9; wiersz++)
+		for (unsigned wiersz = 0; wiersz < 9; wiersz++)
 		{
-			for (unsigned kolumna = 0; kolumna<9; kolumna++)
+			for (unsigned kolumna = 0; kolumna < 9; kolumna++)
 				wyjscie << tablica[i].sudoku[wiersz][kolumna] << "  ";
 
 			wyjscie << "\n";
@@ -274,8 +249,7 @@ void zapisz(Rozwiazania tablica[], unsigned ilosc)
 	cout << "WYNIKI ZOSTALY ZAPISANE W PLIKU O NAZWIE 'sudoku'\n";
 }
 
-void wypelnijMozliweCyfry( vector <MozliweCyfry >& mozliweCyfry, unsigned sudoku[9][9]) {
-
+void wypelnijMozliweCyfry(vector <MozliweCyfry >& mozliweCyfry, unsigned sudoku[9][9]) {
 	for (int wiersz = 0; wiersz < 9; wiersz++) {
 		for (int kolumna = 0; kolumna < 9; kolumna++) {
 			if (!sudoku[wiersz][kolumna]) {
@@ -287,7 +261,6 @@ void wypelnijMozliweCyfry( vector <MozliweCyfry >& mozliweCyfry, unsigned sudoku
 			mozliweCyfry[wiersz * 9 + kolumna].kolumna = kolumna;
 		}
 	}
-	
 }
 
 void wypiszMozliweCyfry(vector <MozliweCyfry >& mozliweCyfry) {
@@ -295,7 +268,7 @@ void wypiszMozliweCyfry(vector <MozliweCyfry >& mozliweCyfry) {
 	for (int wiersz = 0; wiersz < 9; wiersz++) {
 		for (int kolumna = 0; kolumna < 9; kolumna++) {
 			cout << "( " << mozliweCyfry[wiersz * 9 + kolumna].wiersz << " , " << mozliweCyfry[wiersz * 9 + kolumna].kolumna << " ) : ";
-			for (int i = 0 ; i < mozliweCyfry[wiersz * 9 + kolumna].cyfryDoUzycia.size(); i++) {
+			for (int i = 0; i < mozliweCyfry[wiersz * 9 + kolumna].cyfryDoUzycia.size(); i++) {
 				cout << mozliweCyfry[wiersz * 9 + kolumna].cyfryDoUzycia[i];
 			}
 			cout << endl;
@@ -306,90 +279,73 @@ void wypiszMozliweCyfry(vector <MozliweCyfry >& mozliweCyfry) {
 bool sprawdzCzyMozliwe(unsigned sudoku[9][9], unsigned wiersz, unsigned kolumna) {
 	if (wiersz == 999 && kolumna == 999) return true;
 	for (unsigned k = 0; k < 9; k++)
-		if(k!=kolumna)
-		if (sudoku[wiersz][kolumna] == sudoku[wiersz][k])
-			return false;
+		if (k != kolumna)
+			if (sudoku[wiersz][kolumna] == sudoku[wiersz][k])
+				return false;
 
 	for (unsigned w = 0; w < 9; w++)
 		if (w != wiersz)
-		if (sudoku[wiersz][kolumna] == sudoku[w][kolumna])
-			return false;
-
+			if (sudoku[wiersz][kolumna] == sudoku[w][kolumna])
+				return false;
 
 	for (unsigned w2 = (wiersz / 3) * 3; w2 < ((wiersz / 3) * 3 + 3); w2++)
 		for (unsigned k2 = (kolumna / 3) * 3; k2 < ((kolumna / 3) * 3 + 3); k2++)
-			if(w2 != wiersz || k2 != kolumna )
-			if (sudoku[wiersz][kolumna] == sudoku[w2][k2])
-			return false;
+			if (w2 != wiersz || k2 != kolumna)
+				if (sudoku[wiersz][kolumna] == sudoku[w2][k2])
+					return false;
 
 	return true;
-
 }
 
-void minmax(unsigned sudoku[9][9] , unsigned indeksSprawdzanejCyfry, unsigned wstawionyWiersz, unsigned wstawionaKolumna, unsigned long long& LICZBA_ROZWIAZANYCH_SUDOKU, unsigned MAX) {
-	//if (LICZBA_ROZWIAZANYCH_SUDOKU >= MAX) return;
+void minmax(unsigned sudoku[9][9], unsigned indeksSprawdzanejCyfry, unsigned wstawionyWiersz, unsigned wstawionaKolumna, unsigned long long& LICZBA_ROZWIAZANYCH_SUDOKU, unsigned MAX) {
+	if (LICZBA_ROZWIAZANYCH_SUDOKU >= MAX) return;
 
 	if (sprawdzCzyMozliwe(sudoku, wstawionyWiersz, wstawionaKolumna) == false) return;
 
-	if (indeksSprawdzanejCyfry == ILOSC_ELEMENTOW ) {
+	if (indeksSprawdzanejCyfry == ILOSC_ELEMENTOW) {
+		tablica[LICZBA_ROZWIAZANYCH_SUDOKU++] = Rozwiazania(sudoku);
+		if (LICZBA_ROZWIAZANYCH_SUDOKU % 10000 == 0)
+			cout << LICZBA_ROZWIAZANYCH_SUDOKU << "\r";
 
-		//tablica[LICZBA_ROZWIAZANYCH_SUDOKU++] = Rozwiazania(sudoku);
-		LICZBA_ROZWIAZANYCH_SUDOKU++;
-		if(LICZBA_ROZWIAZANYCH_SUDOKU %10000 == 0)
-		cout << LICZBA_ROZWIAZANYCH_SUDOKU << "\r";
-
-		return ;
+		return;
 	}
-
 
 	unsigned sudoku2[9][9];
 
 	nadpisz(sudoku, sudoku2);
 
 	if (mozliweCyfry[indeksSprawdzanejCyfry].cyfryDoUzycia.size() == 0) {
-		
 		minmax(sudoku2, indeksSprawdzanejCyfry + 1, mozliweCyfry[indeksSprawdzanejCyfry].wiersz, mozliweCyfry[indeksSprawdzanejCyfry].kolumna, LICZBA_ROZWIAZANYCH_SUDOKU, MAX);
-
 	}
 	else {
 		for (int j = 0; j < mozliweCyfry[indeksSprawdzanejCyfry].cyfryDoUzycia.size(); j++) {
-
 			sudoku2[mozliweCyfry[indeksSprawdzanejCyfry].wiersz][mozliweCyfry[indeksSprawdzanejCyfry].kolumna] = mozliweCyfry[indeksSprawdzanejCyfry].cyfryDoUzycia[j];
 			minmax(sudoku2, indeksSprawdzanejCyfry + 1, mozliweCyfry[indeksSprawdzanejCyfry].wiersz, mozliweCyfry[indeksSprawdzanejCyfry].kolumna, LICZBA_ROZWIAZANYCH_SUDOKU, MAX);
-
 		}
 	}
-
-		
-
 }
 
 void wyszukajTrudnychSudoku() {
-
 	mt19937 rng;
 	rng.seed(random_device()());
 	uniform_int_distribution<mt19937::result_type> losowyWiersz(0, 8);
 	uniform_int_distribution<mt19937::result_type> losowaKolumna(0, 8);
 
-
 	unsigned sudoku[9][9];
 	wypelnij0(sudoku);
 	unsigned liczbaLosowan = 10000, liczbaNaTablicy = 26, wiersz, kolumna, maxRozwiazan = 10, licznikRozwiazan = 0;
 	for (int i = 0; i < ILOSC_ROZWIAZANYCH_SUDOKU; i++) {
-		if(i%500 == 0)
-		cout << "Przerabiam " << i << " sudoku\r";
+		if (i % 500 == 0)
+			cout << "Przerabiam " << i << " sudoku\r";
 
 		for (int j = 0; j < 10; j++) {
-
 			for (int k = 0; k < liczbaNaTablicy; k++) {
-
 				do {
 					wiersz = losowyWiersz(rng);
 					kolumna = losowaKolumna(rng);
-				} while (sudoku[wiersz][kolumna] != 0 );
+				} while (sudoku[wiersz][kolumna] != 0);
 
 				sudoku[wiersz][kolumna] = ::tablica[i].sudoku[wiersz][kolumna];
-
 			}
 
 			//minmax(sudoku, 0, 999, 999, licznikRozwiazan, maxRozwiazan);
@@ -399,15 +355,12 @@ void wyszukajTrudnychSudoku() {
 
 			licznikRozwiazan = 0;
 			wypelnij0(sudoku);
-
 		}
 	}
 }
 
-void rozwiazywanie(Rozwiazania tablica[], unsigned &ilosc)
+void rozwiazywanie(Rozwiazania tablica[], unsigned& ilosc)
 {
-
-
 	unsigned  sudoku1[9][9], sudokuStale[9][9], Npowt[10], zakres = 0, losowa, kombinacje = 0, tablica1[9][9];
 	unsigned sudoku[9][9] = {
 		{ 0,1,0,0,0,4,0,0,7},
@@ -419,8 +372,6 @@ void rozwiazywanie(Rozwiazania tablica[], unsigned &ilosc)
 		{ 0,4,0,5,3,0,0,0,0},
 		{ 0,0,0,0,0,0,7,0,0},
 		{ 0,0,0,0,0,2,0,8,0}
-
-
 	};
 	cout << "nCzy chcesz miec sudoku wypelnione zerami ? y/n? \n ";
 	char odp;
@@ -429,11 +380,10 @@ void rozwiazywanie(Rozwiazania tablica[], unsigned &ilosc)
 		wypelnij0(sudoku);
 	}
 	else {
-
 		cout << "Wypelnij sudoku, w puste miejsca wstaw 0\n";
 		wypelnij(sudoku);
 	}
-	
+
 	cout << "\n\n\n\nTWOJE SUDOKU \n";
 
 	wypisz(sudoku);
@@ -442,24 +392,19 @@ void rozwiazywanie(Rozwiazania tablica[], unsigned &ilosc)
 
 	wypelnijMozliweCyfry(mozliweCyfry, sudoku);
 
-	minmax(sudoku, 0, 999, 999, ILOSC_ROZWIAZANYCH_SUDOKU, 60000);
+	minmax(sudoku, 0, 999, 999, ILOSC_ROZWIAZANYCH_SUDOKU, 10);
 
 	cout << "Znaleziono : " << ILOSC_ROZWIAZANYCH_SUDOKU << " rozwiazan.\n";
 
 	//wyszukajTrudnychSudoku();
 
-
 	//wypisz(trudneSudoku, 25, 29);
-
-	
-
 
 	_getch();
 
 	wypisz(::tablica);
 
 	nadpisz(sudoku, sudokuStale);
-	
 }
 
 int main()
@@ -482,4 +427,3 @@ int main()
 	return 0;
 }
 // Torso & MojoJojo Production
-
